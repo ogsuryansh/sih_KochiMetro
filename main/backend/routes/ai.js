4f805e52756e6c1676a3c2031716b0c1e2ba9866
ogsuryansh <suryansh1885@gmail.com>
2025-09-12 01:38:43 +0530

    added report

--- main/backend/routes/ai.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { runAIOptimization, runPlanning } = require('../controllers/aiController');
-
-const router = express.Router();
-
-router.post('/optimize', runAIOptimization);
-router.post('/plan/run', runPlanning);
-
-module.exports = router;
